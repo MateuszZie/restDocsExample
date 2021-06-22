aed129948f77869a9f6fbf4b41f9bed658ff284b
Mateusz Ziebura <mateusz_860@op.pl>
2021-06-22 11:45:23 +0200

    Init with initializer

---
+++ src/main/java/org/mateuszziebura/restDocsExample/RestDocsExampleApplication.java
@@ -0,0 +1,13 @@
+package org.mateuszziebura.restDocsExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDocsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDocsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mateuszziebura/restDocsExample/RestDocsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mateuszziebura.restDocsExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDocsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
