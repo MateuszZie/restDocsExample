8763b6b6aaeabfe81572a4433f981910bc6623e0
Mateusz Ziebura <mateusz_860@op.pl>
2021-06-22 14:15:09 +0200

    Spring Mock MVC Configuration Documenting Path Parameters

--- src/test/java/org/mateuszziebura/restDocsExample/web/controller/BeerControllerTest.java
+++ src/test/java/org/mateuszziebura/restDocsExample/web/controller/BeerControllerTest.java
@@ -2,15 +2,18 @@ package org.mateuszziebura.restDocsExample.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mateuszziebura.restDocsExample.domain.Beer;
 import org.mateuszziebura.restDocsExample.repositories.BeerRepository;
 import org.mateuszziebura.restDocsExample.web.model.BeerDto;
 import org.mateuszziebura.restDocsExample.web.model.BeerStyleEnum;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -20,9 +23,15 @@ import java.util.UUID;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
 @ComponentScan(basePackages = "org.mateuszziebura.restDocsExample.web.mappers")
 class BeerControllerTest {
@@ -40,8 +49,9 @@ class BeerControllerTest {
     void getBeerById() throws Exception {
         given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+        .andDo(document("v1/beer", pathParameters(parameterWithName("beerId").description("UUID of desired beer to get."))));
     }
 
     @Test
